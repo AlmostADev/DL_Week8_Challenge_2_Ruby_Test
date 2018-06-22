ca9acfad27e77bb1725657be2e44cdd6f095dfd7
Alejandro Olmos <manuelalejandro.soto1@gmail.com>
2018-06-21 21:32:12 -0400

    Option 2 done

--- Ruby_test.rb
+++ Ruby_test.rb
@@ -17,19 +17,20 @@ def averageAlumnos(data)
     file2.close
 end
 
-generateFile(data)
+averageAlumnos(data)
 
 def inasistenciasAlumnos(data)
     data.each do |item|
         names = item.split(', ')[0]
         marks = item.split(', ')[1..5].each.map(&:to_i)
-        avg = 0
-        marks.each do |mark|
-            avg += mark
-        end
-        promedio = avg / marks.length.to_f
-        file2.puts "#{names} : #{promedio}"
+        asistencia = marks.count(0)
+        puts "#{names} : #{asistencia} asistencia" if asistencia >= 1
     end
 end
 
 inasistenciasAlumnos(data)
+
+def approveStudents()
+
+end
+
